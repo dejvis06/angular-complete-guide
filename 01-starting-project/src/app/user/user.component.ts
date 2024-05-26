b863eafb35448f570ebdc8d26f1686fa1104dbf5
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-05-26 10:36:04 +0200

    create shared div container

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -15,10 +15,12 @@ import {
 }; */
 
 import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
