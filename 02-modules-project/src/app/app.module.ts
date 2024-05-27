d2f8c326d12bf0270d176756fe8575ae11c9502b
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-05-27 11:25:10 +0200

    convert to stand alone components to modules and create a shared module

--- 02-modules-project/src/app/app.module.ts
+++ 02-modules-project/src/app/app.module.ts
@@ -4,10 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { HeaderComponent } from './header/header.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
+  imports: [BrowserModule, FormsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
