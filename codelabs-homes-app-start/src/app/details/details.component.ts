d12aa0e8c91739980f62b503eff4aad0dcea94b7
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2024-06-09 15:08:59 +0200

    create submission form

--- codelabs-homes-app-start/src/app/details/details.component.ts
+++ codelabs-homes-app-start/src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
 import { HousingService } from "../housing.service";
 import { HousingLocation } from "../housinglocation";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-details",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: "./details.component.html",
   styleUrls: ["./details.component.css"],
 })
@@ -17,10 +18,24 @@ export class DetailsComponent {
   housingLocation?: HousingLocation;
   housingService: HousingService = inject(HousingService);
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+  });
+
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params["id"]);
     this.housingLocation = this.housingService.getHousingLocationById(
       this.housingLocationId
+    );
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? "",
+      this.applyForm.value.lastName ?? "",
+      this.applyForm.value.email ?? ""
     );
   }
 }
